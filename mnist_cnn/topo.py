9ec485d75470bd35e92a6e9a34246c59bf6bc767
Linda Cai <tcai4@illinois.edu>
2018-05-11 02:21:40 +0000

    inception and topo

--- mnist_cnn/topo.py
+++ mnist_cnn/topo.py
@@ -1,17 +1,19 @@
+import networkx
 import itertools  
 import operator
 import heapq
 import math
 
 def topo(G, P):
-    sorted_nodes = topological_sort(G)
+    sorted_nodes = list(networkx.topological_sort(G))
     num_proc = len(P)
-    num_nodes - G.number_of_nodes()
-    nodes_per_proc = math.ceil(float(num_nodes)/num_proc)
+    num_nodes = G.number_of_nodes()
+    nodes_per_proc = int(math.ceil(float(num_nodes)/num_proc))
     Nodes = G.nodes
-    for num in num_proc:
+    for num in xrange(num_proc):
         lo = nodes_per_proc*num
         hi = min(num_nodes, nodes_per_proc*(num+1))
-        for ID in (lo, hi, 1):
-            Nodes[sorted_nodes[ID]['id']]['p'] = num
+        for ID in range(lo, hi, 1):
+            curr_id = sorted_nodes[ID]
+            G.nodes[curr_id]['p'] = num
     return G
