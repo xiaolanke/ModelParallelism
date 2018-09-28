ff304f94c15ceac3a6890a2fda9d48ac1d841f68
Jintao Jiang <jjiang43@illinois.edu>
2018-09-28 11:26:00 -0500

    Update setup.sh

--- inception_net/setup.sh
+++ inception_net/setup.sh
@@ -7,7 +7,6 @@ sudo apt-get install openjdk-8-jdk
 echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
 curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
 sudo apt-get update && sudo apt-get install bazel
-git clone https://github.com/lindaCai1997/ModelParallelism.git
 git checkout dist
 cd ModelParallelism/inception_net
 FLOWERS_DATA_DIR=/tmp/flowers-data/
