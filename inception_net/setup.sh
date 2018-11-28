361a2d3bc18bec90763831a8237f52ce021ab8a4
Xiaolan Ke <xiaolan2@illinois.edu>
2018-11-27 23:03:29 -0600

    s

--- inception_net/setup.sh
+++ inception_net/setup.sh
@@ -1,8 +1,8 @@
 sudo apt-get update
-sudo apt-get install python-pip
-pip install tensorflow
-pip install networkx
-pip install matplotlib
+sudo apt-get install python-pip3
+pip3 install tensorflow
+pip3 install networkx
+pip3 install matplotlib
 sudo apt-get install openjdk-8-jdk
 echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
 curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
